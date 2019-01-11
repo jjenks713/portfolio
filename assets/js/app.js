00ce5259526e56b569c93e8a548c2c0db7a22ef2
Chillen <josh.jenkin@live.com>
2019-01-11 15:43:41 -0700

    carousel working, switched to toggle jQuery

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,20 @@
-  $(document).ready(function(){
-    $('.carousel').carousel();
-    $('.carousel-slider').slider({full_width: true});
-    $('.slider').slider({full_width: true});
-    $('.collapsible').collapsible();
-    $('.dropdown-trigger').dropdown();
-    $('#about').hide();
-    $('#about-2').hide();
-  });
-  $('#about-btn').click(function(){
-      $('#about').show();
-  });
-  $('#about-btn-2').click(function(){
-    $('#about-2').show();
+$(document).ready(function () {
+  $('.carousel').carousel();
+  $('.carousel-slider').slider({ full_width: true });
+  $('.slider').slider({ full_width: true });
+  $('#about').hide();
+  $('#about-2').hide();
 });
+
+$('#about-btn').click(function () {
+  $('#about').toggle();
+});
+
+$('#about-btn-2').click(function () {
+  $('#about-2').toggle();
+});
+
 $('.carousel.carousel-slider').carousel({
   fullWidth: true
 });
-      
+
