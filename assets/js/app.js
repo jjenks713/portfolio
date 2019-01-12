e335b4ac4f0c4fe0439a80d77409447f26e3b580
Chillen <josh.jenkin@live.com>
2019-01-12 10:43:07 -0700

    added padding to ab div for responsive cleanness

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,34 +6,40 @@ $(document).ready(function () {
   $('#about-2').hide();
 });
 
+$('#about-btn').click(function () {
+  $('#about').toggle();
+});
 
+$('#about-btn-2').click(function () {
+  $('#about-2').toggle();
+});
 
 $('.carousel.carousel-slider').carousel({
   fullWidth: true
 });
 
-function myFunction(x) {
-  if (x.matches) { // If media query matches
-    $('#about-btn').click(function () {
-      $('#about').toggle();
-      $('#about-btn-2').toggle();
-    });
+// function myFunction(x) {
+//   if (x.matches) { // If media query matches
+//     $('#about-btn').click(function () {
+//       $('#about').toggle();
+//       $('#about-btn-2').toggle();
+//     });
     
-    $('#about-btn-2').click(function () {
-      $('#about-2').toggle();
-      $('#about-btn').toggle();
-    });
-  } else {
-    $('#about-btn').click(function () {
-      $('#about').toggle();
-    });
+//     $('#about-btn-2').click(function () {
+//       $('#about-2').toggle();
+//       $('#about-btn').toggle();
+//     });
+//   } else {
+//     $('#about-btn').click(function () {
+//       $('#about').toggle();
+//     });
     
-    $('#about-btn-2').click(function () {
-      $('#about-2').toggle();
-    });
-  }
-}
+//     $('#about-btn-2').click(function () {
+//       $('#about-2').toggle();
+//     });
+//   }
+// }
 
-var x = window.matchMedia("(max-width: 450px)")
-myFunction(x) // Call listener function at run time
-x.addListener(myFunction) // Attach listener function on state changes
+// var x = window.matchMedia("(max-width: 450px)")
+// myFunction(x) // Call listener function at run time
+// x.addListener(myFunction) // Attach listener function on state changes
