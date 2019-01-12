af9f4c947cddd5c7d3dc1a84608cd18cd0d32b90
Chillen <josh.jenkin@live.com>
2019-01-12 11:43:45 -0700

    cleaned up code, deleted unused JS attempt

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,29 +17,3 @@ $('#about-btn-2').click(function () {
 $('.carousel.carousel-slider').carousel({
   fullWidth: true
 });
-
-// function myFunction(x) {
-//   if (x.matches) { // If media query matches
-//     $('#about-btn').click(function () {
-//       $('#about').toggle();
-//       $('#about-btn-2').toggle();
-//     });
-    
-//     $('#about-btn-2').click(function () {
-//       $('#about-2').toggle();
-//       $('#about-btn').toggle();
-//     });
-//   } else {
-//     $('#about-btn').click(function () {
-//       $('#about').toggle();
-//     });
-    
-//     $('#about-btn-2').click(function () {
-//       $('#about-2').toggle();
-//     });
-//   }
-// }
-
-// var x = window.matchMedia("(max-width: 450px)")
-// myFunction(x) // Call listener function at run time
-// x.addListener(myFunction) // Attach listener function on state changes
